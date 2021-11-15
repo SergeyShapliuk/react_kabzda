1791f4248fcb13747619c200237c3a42d9fa43ea
Sergey <sergeshapluk@gmail.com>
2021-11-15 18:36:31 +0300

    select

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Rating, {RatingValueType} from "./component/Rating/Rating";
 
 function App() {
+    let[ratingValue,setRatingValue]=useState<RatingValueType>(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Rating value={ratingValue} onClick={setRatingValue}/>
     </div>
   );
 }
