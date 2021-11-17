32a4053457d1740a8f7e218241b5fee48bc76f94
Sergey <sergeshapluk@gmail.com>
2021-11-17 19:53:29 +0300

    reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
 import Rating, {RatingValueType} from "./component/Rating/Rating";
+import {UncontrolledAccordion} from "./component/UncontroledAcccordion/UncontrolledAccordion";
 
 function App() {
     let[ratingValue,setRatingValue]=useState<RatingValueType>(0)
   return (
     <div className="App">
+        <UncontrolledAccordion title={"Menu"}/>
         <Rating value={ratingValue} onClick={setRatingValue}/>
     </div>
   );
---
+++ src/component/UncontroledAcccordion/UncontrolledAccordion.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {UncontrolledAccordion} from "./UncontrolledAccordion";
+
+
+export default {
+    title:"UncontrolledAccordion",
+    component:UncontrolledAccordion
+}
+
+export const UncontrolledAccordionValue=()=>{
+    return <>
+    <UncontrolledAccordion title={"Menu"}/>
+    </>
+}
