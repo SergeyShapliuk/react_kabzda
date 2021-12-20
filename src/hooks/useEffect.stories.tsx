ae048eae35039f6753e7d2b74e4870b5483223d2
Sergey <sergeshapluk@gmail.com>
2021-12-20 12:25:12 +0300

    useEffect

---
+++ src/hooks/useEffect.stories.tsx
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from "react";
+
+
+export default {
+    title: 'useEffect'
+}
+
+
+export function Example() {
+    const [fake, setFake] = useState(0)
+    const [count, setCount] = useState(0)
+    console.log('Example')
+
+    useEffect(() => {
+        console.log('useEffect all ready')
+        document.title = count.toString()
+    })
+    useEffect(() => {
+        console.log('useEffect only one')
+        document.title = count.toString()
+    }, [])
+    useEffect(() => {
+        console.log('useEffect only count change')
+        document.title = count.toString()
+    }, [count])
+
+    return <>
+        count: {count} fake: {fake}
+        <button onClick={() => setCount(count + 1)}>count</button>
+        <button onClick={() => setFake(fake + 1)}>fake</button>
+
+    </>
+}
+
+export function SetTimeOutExample() {
+    const [fake, setFake] = useState(0)
+    const [count, setCount] = useState(0)
+    console.log('Example')
+    useEffect(() => {
+        console.log('useEffect')
+        setInterval(() => {
+            console.log('setTimeOut')
+
+            document.title = count.toString()
+        }, 1000)
+
+
+    },[count])
+
+
+    return <>
+        count: {count} fake: {fake}
+        {/*<button onClick={()=>setCount(count+1)}>count</button>*/}
+        {/*<button onClick={()=>setFake(fake+1)}>fake</button>*/}
+
+    </>
+}
